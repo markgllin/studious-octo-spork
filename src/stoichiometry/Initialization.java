def9fa18972176931e4129cc3474c094bff3e0b8
markgllin <markgllin@hotmail.com>
2016-04-03 00:00:29 -0600

    working balancer

--- src/stoichiometry/Initialization.java
+++ src/stoichiometry/Initialization.java
@@ -16,22 +16,31 @@ public class Initialization {
 	private static String dataFile = "C:\\Users\\marklin\\Desktop\\pt-data2.txt";
 	
 	public static void main(String args[]) throws IOException{
-		
-		/*
+
         //Creating  Arrays Representing Equations
         double[][] lhsArray = {{1,0,0,-1,0,0,0,0}       //Cu
-        					  ,{1,0,0,-1,0,0,0,0}		//S
-        					  ,{1,0,0,0,0,-1,0,0}		//C
-        					  ,{1,0,0,0,0,-1,0,0}		//N
-        					  ,{0,1,0,0,-1,0,0,0}		//K
-        					  ,{0,1,0,0,0,0,-1,0}		//I
-        					  ,{0,3,0,-4,0,0,0,-1}		//O
-        					  ,{0,0,1,0,0,-1,0,-2}		//H
-        					  ,{0,0,1,0,-1,0,-1,0}		//Cl
-        					  ,{1,0,0,0,0,0,0,0}		//arbitrary
-        					  };
+							  ,{1,0,0,-1,0,0,0,0}		//S
+							  ,{1,0,0,0,0,-1,0,0}		//C
+							  ,{1,0,0,0,0,-1,0,0}		//N
+							  ,{0,1,0,0,-1,0,0,0}		//K
+							  ,{0,1,0,0,0,0,-1,0}		//I
+							  ,{0,3,0,-4,0,0,0,-1}		//O
+							  ,{0,0,1,0,0,-1,0,-2}		//H
+							  ,{0,0,1,0,-1,0,-1,0}		//Cl
+							  ,{1,0,0,0,0,0,0,0}		//arbitrary
+							  };
         double[] rhsArray = {0,0,0,0,0,0,0,0,0,1};
- 
+
+        
+		
+		//2x + y + 3z = 1
+		//2x + 6y + 8z = 3
+		//6x + 8y + 18z = 5
+		
+		//double[][] lhsArray = {{2,1,3}, {2,6,8},{6,8,18}};
+		//double[] rhsArray = {1,3,5};
+        List<Integer> coef = new ArrayList<Integer>();
+        
         //Creating Matrix Objects with arrays
         Matrix lhs = new Matrix(lhsArray);
         Matrix rhs = new Matrix(rhsArray, rhsArray.length);
@@ -41,11 +50,17 @@ public class Initialization {
  
         //Printing Answers
         for (int i=0;i<8;i++)
-        	System.out.println(ans.get(i, 0));
-        */
+        	coef.add((int)(100000*(float)ans.get(i, 0)));
+        	//System.out.println((float)ans.get(i, 0));
+        
+        
+		//System.out.println("test");
+		//System.out.println(new Rational(3.75));
 		
-		int gcd = findGCD(new ArrayList<Integer>(Arrays.asList(100,1000,10000,100000)));
+		int gcd = findGCD(coef);
 		System.out.println("GCD:"+gcd);
+        for (int i=0;i<8;i++)
+        	System.out.println(coef.get(i)/gcd);
 	/*
 		float mass, percentMass;
 		Compound compound;
@@ -82,7 +97,7 @@ public class Initialization {
 		List<Integer> even = new ArrayList<Integer>();
 		List<Integer> noDupe = new ArrayList<Integer>();
 		int count=0, smallest = 1000000000, mult=1;
-
+			
 		if (numbers.size()==1) return numbers.get(0);
 		
 		//if anything in S is 0, remove
@@ -97,29 +112,26 @@ public class Initialization {
 					odd.add(i);
 				}
 		}
-		
+
 		//if all numbers even, divide everything by 2 and set multiplier
 		if (count==even.size()){
 			for (int i: even)
 				posInts.add(i/2);
 			mult *= 2;
-			System.out.println("All even:");
+		//if all numbers odd, (n-k)/2 where k=smallest number and n = all other numbers
 		}else if (count==odd.size()){
-			for (int i =1; i<odd.size();i++)
-				if(odd.get(i)!=smallest)
-					posInts.add((odd.get(i)-smallest)/2);
+			for (int i : odd)
+				if(i!=smallest)
+					posInts.add((i-smallest)/2);
 				else
-					posInts.add(odd.get(i));
-			System.out.println("All odd:");
+					posInts.add(i);
+		//if mix of odd/even, divide all even by 2
 		}else{
 			for(int i:even)
 				odd.add(i/2);
 			posInts = odd;
-			System.out.println("Mix:");
 		}
-		for (int i:posInts)
-			System.out.println(i);
-		
+
 		for (int i:posInts)
 			if(!noDupe.contains(i))
 				noDupe.add(i);
